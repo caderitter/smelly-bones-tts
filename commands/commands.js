239481056fcfb741e1fde90ee6452d7de75dacc5
Cade Ritter <cade.ritter@ridewithvia.com>
2023-06-09 18:19:03 -0400

    adds ability to change voice type

--- commands/commands.js
+++ commands/commands.js
@@ -1,11 +1,15 @@
 export default [
   {
-    name: 'starttts',
+    name: "starttts",
     description:
       "joins the channel you're in and speaks every message said in #no-mic",
   },
   {
-    name: 'stoptts',
-    description: 'leaves the channel',
+    name: "stoptts",
+    description: "leaves the channel",
+  },
+  {
+    name: "setvoice",
+    description: "sets the selected voice of the bot",
   },
 ];
--- tts/tts.js
+++ tts/tts.js
@@ -1,18 +1,23 @@
-import { createAudioResource } from '@discordjs/voice';
-import textToSpeech from '@google-cloud/text-to-speech';
-import { Readable } from 'stream';
+import { createAudioResource } from "@discordjs/voice";
+import textToSpeech from "@google-cloud/text-to-speech";
+import { Readable } from "stream";
 
 const client = new textToSpeech.TextToSpeechClient();
 
-export const synthesize = async text => {
+export const synthesize = async (text, voiceName) => {
   const request = {
     input: { text },
-    voice: { languageCode: 'en-UK', ssmlGender: 'FEMALE' },
-    audioConfig: { audioEncoding: 'MP3' },
+    voice: { name: voiceName, languageCode: voiceName.slice(0, 5) },
+    audioConfig: { audioEncoding: "MP3" },
   };
 
   const [{ audioContent }] = await client.synthesizeSpeech(request);
   const stream = Readable.from(audioContent);
   const resource = createAudioResource(stream);
   return resource;
+};
+
+export const listVoices = async () => {
+  const voicesResponse = await client.listVoices({ languageCode: "en" });
+  return voicesResponse[0].voices;
 };

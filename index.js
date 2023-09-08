a7007143b6a3051583bb5a7c28b9c5393846ac28
caderitter <ritter.cade@gmail.com>
2023-09-08 01:20:09 -0400

    it works!

--- index.js
+++ index.js
@@ -5,35 +5,35 @@ import {
   GatewayIntentBits,
   StringSelectMenuBuilder,
   StringSelectMenuOptionBuilder,
-} from "discord.js";
+} from 'discord.js';
 import {
   AudioPlayerStatus,
   createAudioPlayer,
   getVoiceConnection,
   joinVoiceChannel,
-} from "@discordjs/voice";
+} from '@discordjs/voice';
 
-import config from "./config.json" assert { type: "json" };
-import commands from "./commands/commands.js";
-import registerCommands from "./commands/registerCommands.js";
-import { listVoices, synthesize } from "./tts/tts.js";
-import { ComponentType } from "discord.js";
-import { scheduleJob } from "node-schedule";
-import { readFile, writeFile } from "node:fs/promises";
+import config from './config.json' assert { type: 'json' };
+import commands from './commands/commands.js';
+import registerCommands from './commands/registerCommands.js';
+import { listVoices, synthesize } from './tts/tts.js';
+import { ComponentType } from 'discord.js';
+import { scheduleJob } from 'node-schedule';
+import { readFile, writeFile } from 'node:fs/promises';
 
 let listening = false;
 let channelName;
 let subscription;
 let queue = [];
 let playing = false;
-let selectedVoice = "en-US-News-N";
+let selectedVoice = 'en-US-News-N';
 
 (async () => {
   // register slash commands
   try {
     await registerCommands(commands);
   } catch (e) {
-    console.error("Error registering commands: ", e);
+    console.error('Error registering commands: ', e);
   }
 
   const client = new Client({
@@ -45,21 +45,20 @@ let selectedVoice = "en-US-News-N";
     ],
   });
 
-  scheduleJob("* * * * *", async () => {
-    const jsonString = await readFile("./birthdays.json", {
-      encoding: "utf-8",
+  scheduleJob('* * * * *', async () => {
+    const jsonString = await readFile('./birthdays.json', {
+      encoding: 'utf-8',
     });
     const birthdaysObject = JSON.parse(jsonString);
     const today = new Date();
     const day = today.getDate().toString();
     const month = today.getMonth().toString();
 
-
     Object.entries(birthdaysObject).forEach(([userMentionString, birthday]) => {
       console.log(birthday, day, month);
       const [storedMonth, storedDay] = birthday.split('/');
       if (storedDay === day && storedMonth === month) {
-        const channel = client.channels.cache.get("935746352502173777");
+        const channel = client.channels.cache.get('935746352502173777');
         channel.send(`Happy birthday ${userMentionString}!!!!`);
       }
     });
@@ -80,13 +79,13 @@ let selectedVoice = "en-US-News-N";
     playing = true;
   });
 
-  client.on(Events.InteractionCreate, async (interaction) => {
+  client.on(Events.InteractionCreate, async interaction => {
     if (!interaction.isChatInputCommand()) return;
-    if (interaction.channel.name !== "no-mic") return;
-    if (interaction.commandName === "starttts") {
+    if (interaction.channel.name !== 'no-mic') return;
+    if (interaction.commandName === 'starttts') {
       await interaction.deferReply();
       if (!interaction.member.voice.channel) {
-        await interaction.editReply("You must be in a voice channel");
+        await interaction.editReply('You must be in a voice channel');
         return;
       }
 
@@ -102,19 +101,19 @@ let selectedVoice = "en-US-News-N";
         channelName = interaction.member.voice.channel.name;
       }
       interaction.editReply(
-        "joined your channel and started listening to #no-mic"
+        'joined your channel and started listening to #no-mic'
       );
     }
 
-    if (interaction.commandName === "stoptts") {
+    if (interaction.commandName === 'stoptts') {
       if (!interaction.member.voice.channel) {
-        await interaction.reply("You must be in a voice channel");
+        await interaction.reply('You must be in a voice channel');
         return;
       }
 
       let connection = getVoiceConnection(interaction.guildId);
       if (!connection) {
-        await interaction.reply("tts is not enabled");
+        await interaction.reply('tts is not enabled');
         return;
       }
 
@@ -124,40 +123,40 @@ let selectedVoice = "en-US-News-N";
       listening = false;
       playing = false;
       channelName = null;
-      interaction.reply("left the channel and stopped listening to #no-mic");
+      interaction.reply('left the channel and stopped listening to #no-mic');
     }
 
-    if (interaction.commandName === "setvoice") {
+    if (interaction.commandName === 'setvoice') {
       await interaction.deferReply();
       let voices = [];
       try {
         voices = await listVoices();
       } catch (e) {
         await interaction.editReply({
-          content: "There was an error fetching voices: " + e,
+          content: 'There was an error fetching voices: ' + e,
         });
         return;
       }
 
       const rows = chunkArray(voices, 25).map((chunk, i) => {
-        const options = chunk.map((voice) =>
+        const options = chunk.map(voice =>
           new StringSelectMenuOptionBuilder()
             .setLabel(`${voice.name} - ${voice.ssmlGender}`)
             .setValue(voice.name)
         );
         const selectMenu = new StringSelectMenuBuilder()
-          .setCustomId("voice select" + i)
-          .setPlaceholder("Select a voice...")
+          .setCustomId('voice select' + i)
+          .setPlaceholder('Select a voice...')
           .addOptions(options);
         return new ActionRowBuilder().addComponents(selectMenu);
       });
 
       const response = await interaction.editReply({
-        content: "Choose a voice from one of the lists",
+        content: 'Choose a voice from one of the lists',
         components: [...rows],
       });
 
-      const collectorFilter = (i) => i.user.id === interaction.user.id;
+      const collectorFilter = i => i.user.id === interaction.user.id;
 
       try {
         const selection = await response.awaitMessageComponent({
@@ -172,61 +171,65 @@ let selectedVoice = "en-US-News-N";
         });
       } catch (e) {
         await interaction.editReply({
-          content: "Selection was not made within 1 minute, aborting",
+          content: 'Selection was not made within 1 minute, aborting',
           components: [],
         });
       }
     }
 
-    if (interaction.commandName === "setbirthday") {
-      const user = interaction.options.getUser("user");
+    if (interaction.commandName === 'setbirthday') {
+      const user = interaction.options.getUser('user');
       const userMentionString = user.toString();
 
-      const dateString = interaction.options.getString("date");
+      const dateString = interaction.options.getString('date');
       const dateRegex = /^(1[0-2]|[1-9])\/(3[01]|[12][0-9]|[1-9])$/;
-      if (!dateRegex.match(dateString)) {
-        await interaction.editReply("That date is not in valid MM/DD format. Remember to not include leading zeros.");
+      if (!dateString.match(dateRegex)) {
+        await interaction.editReply(
+          'That date is not in valid MM/DD format. Remember to not include leading zeros.'
+        );
         return;
       }
 
+      await interaction.deferReply();
+
       try {
-        const jsonString = await readFile("./birthdays.json", {
-          encoding: "utf-8",
+        const jsonString = await readFile('./birthdays.json', {
+          encoding: 'utf-8',
         });
         const birthdaysObject = JSON.parse(jsonString);
         const newBirthdaysObject = {
           ...birthdaysObject,
           [userMentionString]: dateString,
         };
-        await writeFile("./birthdays.json", JSON.stringify(newBirthdaysObject));
+        await writeFile('./birthdays.json', JSON.stringify(newBirthdaysObject));
         await interaction.editReply(`✅ added ${user}'s birthday`);
       } catch (e) {
-        await interaction.editReply("There was an error: " + e);
+        await interaction.editReply('There was an error: ' + e);
       }
     }
   });
 
-  client.on(Events.MessageCreate, async (message) => {
+  client.on(Events.MessageCreate, async message => {
     if (!message.guildId) return;
     if (!listening) return;
-    if (message.channel.name !== "no-mic") return;
+    if (message.channel.name !== 'no-mic') return;
     if (!message.member.voice.channel) return;
     if (message.member.voice.channel.name !== channelName) return;
     if (message.member.id === config.clientId) return;
     if (message.content.length > 100) {
-      await message.reply("that message is too long");
+      await message.reply('that message is too long');
       return;
     }
     if (!message.member.voice.selfMute) return;
 
-    const username = message.member.user.username.split("#")[0];
+    const username = message.member.user.username.split('#')[0];
     const contentWithReadableEmojis = message.content.replace(
       /<:(.+?):\d+>/,
-      "$1"
+      '$1'
     );
     const contentWithNoUrls = contentWithReadableEmojis.replace(
       /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/,
-      ""
+      ''
     );
 
     if (!contentWithNoUrls) return;
@@ -240,12 +243,12 @@ let selectedVoice = "en-US-News-N";
         queue.push(resource);
       }
     } catch (e) {
-      console.error("There was an error: ", e);
+      console.error('There was an error: ', e);
     }
   });
 
   await client.login(config.token);
-  console.log("ready");
+  console.log('ready');
 })();
 
 function chunkArray(arr, chunkSize) {
